b0441af30e95d16ee235846f55454412aa6fcdf3
lilia.simeonova <lilia.valentinova.simeonova@gmail.com>
2017-02-03 15:17:55 +0200

    improve algorithm

--- app/index.js
+++ app/index.js
@@ -5,52 +5,66 @@ var jaccard = require('jaccard-similarity-sentences');
 
 var getSentencesFromArticle = require('get-sentences-from-article');
 
-exports.summaryWithQuestion = function(question, text, numberSentences) {
-	var weigthedSentences = new Array();
-	var sentences = getSentencesFromArticle(text);
-	if(sentences) {
-		for(var i = 0; i < sentences.length; i++) {
-				weigthedSentences.push({
-						i:i, 
-						sentence: sentences[i], 
-						weight:jaccard.jaccardSimilarity(question, sentences[i])
-					});
-			}
-			var sortedArray = weigthedSentences.sort(function(a, b) { 
-				return b['weight'] - a['weight'];
-			});
+exports.summaryWithQuestions = function(question, text, numberSentences) {
+  var weigthedSentences = new Array();
+  var sentences = getSentencesFromArticle(text);
+  if (sentences) {
+    for (var i = 0; i < sentences.length; i++) {
+      weigthedSentences.push({
+        index: i,
+        sentence: sentences[i],
+        weight: jaccard.jaccardSimilarity(question, sentences[i])
+      });
+    }
+    var sortedArray = weigthedSentences.sort(function(a, b) {
+      return b.weight - a.weight;
+    })
+    .slice(0, numberSentences)
+    .sort(function (a, b) {
+      return a.index - b.index;
+    });
 
-			var result = '';
-			for(var i = 0; i < numberSentences; i++) {
-				result = result + sortedArray[i]['sentence'] + ' ';
-			}
-			return result;
-		} else {
-			return []
-		}
+    var result = '';
+    for(var i = 0; i < sortedArray.length; i++) {
+      result = result + sortedArray[i]['sentence'] + ' ';
+    }
+    return result;
+  } else {
+    return []
+  }
 }
 
 exports.summary = function(text, numberSentences) {
-	var weigthedSentences = new Array();
-	var sentences = getSentencesFromArticle(text);
-	if(sentences) {
-		for(var i = 0; i < sentences.length; i++) {		
-			weigthedSentences.push({
-					i:i, 
-					sentence: sentences[i], 
-					weight: jaccard.jaccardSimilarity(sentences[0], sentences[i])
-				});
-			}
-			var sortedArray = weigthedSentences.sort(function(a, b) { 
-				return b['weight'] - a['weight'];
-			});
+  var weigthedSentences = new Array();
+  var sentences = getSentencesFromArticle(text);
+  if (sentences) {
+    for (var i = 0; i < sentences.length; i++) {
+      weigthedSentences.push({
+        index: i,
+        sentence: sentences[i],
+        weight: jaccard.jaccardSimilarity(sentences[0], sentences[i])
+      });
+    }
+    var sortedArray = weigthedSentences.sort(function(a, b) {
+      return b.weight - a.weight;
+    })
+    .slice(0, numberSentences)
+    .sort(function (a, b) {
+      return a.index - b.index;
+    });
 
-			var result = '';
-			for(var i = 0; i < numberSentences; i++) {
-				result = result + sortedArray[i]['sentence'] + ' ';
-			}
-			return result;
-	} else {
-		return []
-	}
+    var result = '';
+    for(var i = 0; i < sortedArray.length; i++) {
+      result = result + sortedArray[i]['sentence'] + ' ';
+    }
+    return result;
+  } else {
+    return []
+  }
 }
+
+var fs = require('fs');
+
+var text = fs.readFileSync('../test/news.txt', 'utf8').toString();
+
+console.log(exports.summaryWithQuestions('when trump became presidential?',text, 3));
